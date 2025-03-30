0f64dadfcd6da69cbc51239d551553b3157474cb
liujeremy67 <118013349+liujeremy67@users.noreply.github.com>
2025-03-30 14:40:11 -0400

    Update myheart.py

--- mapreduce/manager/myheart.py
+++ mapreduce/manager/myheart.py
@@ -3,9 +3,13 @@ import socket
 import json
 import logging
 import time
-from mapreduce.manager.job import *
+from mapreduce.manager.job import Job
+
+LOGGER = logging.getLogger(__name__)
+
 
 def manager_udp_server(self, host, port):
+    """Create the heartbeat listener."""
     # Create an INET, DGRAM socket, this is UDP
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
 
@@ -32,40 +36,47 @@ def manager_udp_server(self, host, port):
                     worker = (worker_host, worker_port)
                     handle_heartbeat(self, worker)
                 else:
-                    LOGGER.error(f"!!!wrong message type on heartbeat line!!!")
+                    LOGGER.error("!!!wrong message type on heartbeat line!!!")
 
-            except UnicodeDecodeError as e:
-                LOGGER.error(f"Failed to decode message from {host}:{port}. Error: {e}")
+            except UnicodeDecodeError:
+                # LOGGER.error(f"Failed to decode message
+                # from {host}:{port}. Error: {e}")
                 continue
-            except json.JSONDecodeError as e:
-                LOGGER.error(f"Failed to parse JSON from message: {message_str}. Error: {e}")
+            except json.JSONDecodeError:
+                # LOGGER.error(f"Failed to parse JSON from message:
+                # {message_str}. Error: {e}")
                 continue
-            except:
-                LOGGER.error(f"failed heartbeat processing for some other reason")
+
 
 def handle_heartbeat(self, worker):
+    """If heartbeat is received then process it."""
     current_time = time.time()
     if worker not in self.workers:
-        LOGGER.info(f"heartbeat from unregistered {worker} skipped (spec)")
+        # LOGGER.info(f"heartbeat from unregistered {worker} skipped (spec)")
         return
     if worker in self.dead_workers:
-        LOGGER.info(f"Worker {worker} revived.")
+        # LOGGER.info(f"Worker {worker} revived.")
         self.dead_workers.remove(worker)
 
     self.worker_heartbeats[worker] = current_time
-    LOGGER.info(f"Received heartbeat from worker {worker} at {current_time}")
+    # LOGGER.info(f"Received heartbeat from worker {worker} at {current_time}")
+
 
 # below run on sep thread
 def check_heartbeats(self):
+    """Check heartbeats every few seconds."""
     while not self.shutdown_event.is_set():
         current_time = time.time()
         for worker, last_heartbeat in self.worker_heartbeats.items():
             if current_time - last_heartbeat > 10:
-                LOGGER.warning(f"Worker {worker} has missed {5} heartbeats and is assumed dead.")
+                # LOGGER.warning(f"Worker {worker} has missed {5}
+                # heartbeats and is assumed dead.")
                 handle_dead_worker(self, worker)
         time.sleep(2)
 
+
 def handle_dead_worker(self, worker):
+    """Deal with dead workers and reassign tasks."""
     # marking dead
     if worker in self.worker_heartbeats:
         del self.worker_heartbeats[worker]
@@ -76,11 +87,16 @@ def handle_dead_worker(self, worker):
         del self.busy_workers[worker]
 
         # find its task id
-        matching_task_ids = [id for id, val in self.job.in_progress_tasks.items() if val == worker]
-        LOGGER.info(f"Matching task ids for dead worker (should only be 1): {matching_task_ids}")
+        matching_task_ids = [id for id, val in
+                             self.job.in_progress_tasks.items()
+                             if val == worker]
+        # LOGGER.info(f"Matching task ids for dead worker (should only be 1):
+        #  {matching_task_ids}")
         matching_id = matching_task_ids[0]
 
         # need to reassign
-        task_dict = self.job.task_reference_dict[matching_id] # corresponding task msg
-        LOGGER.info(f"Reassigning task {task_dict}. Should have task_id in here")
+        task_dict = self.job.task_reference_dict[matching_id]
+        # corresponding task msg
+        # LOGGER.info(f"Reassigning task {task_dict}.
+        # Should have task_id in here")
         self.job.reset_task(task_dict)
