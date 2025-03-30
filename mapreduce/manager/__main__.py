ed216faf3b03ddfa31180ec0e9046e3195f4cbd7
ssaze <saze@umich.edu>
2025-03-30 14:40:57 -0400

    style

--- mapreduce/manager/__main__.py
+++ mapreduce/manager/__main__.py
@@ -1,36 +1,43 @@
-import os
+"""Manager."""
 import sys
 import tempfile
 import logging
-import json
-import time
 import threading
-import socket
-import pathlib
-import shutil
-import click
-
-from pathlib import Path
 from collections import deque
 
-from mapreduce.utils.ordered_dict import *
-from mapreduce.manager.tcp_listener import *
-from mapreduce.manager.myheart import *
+import click
+
+from mapreduce.utils.ordered_dict import OrderedDict, ThreadSafeOrderedDict
+from mapreduce.manager.tcp_listener import manager_tcp_server
+from mapreduce.manager.myheart import manager_udp_server, check_heartbeats
 from mapreduce.manager.fuckingmaininputmapreduce import job_queue
-
-
-#TODO ignore misbehaving workers
-
+from mapreduce.utils.servers import tcp_client
 
 # Configure logging
 LOGGER = logging.getLogger(__name__)
+
+
+class WorkerRegistry:
+    def __init__(self):
+        self.workers = OrderedDict()
+        self.busy_workers = OrderedDict()
+        self.worker_heartbeats = ThreadSafeOrderedDict()
+        self.dead_workers = set()
+
+class JobQueueManager:
+    def __init__(self):
+        self.job = None
+        self.job_queue = deque()
+        self.next_job_id = 0
+        self.new_job_alert_condition = threading.Condition()
+
 
 class Manager:
     """Represent a MapReduce framework Manager node."""
 
     def __init__(self, host, port):
-        "ohhhhh i wish the lord would take me now"
-        # variable inits
+        """Initialize Manager."""
+        
         self.host = host
         self.port = port
         self.job = None
@@ -46,52 +53,70 @@ class Manager:
         self.job_queue = deque()
         self.next_job_id = 0
 
-        # Create temporary directory for shared map/reduce files
-        prefix = f"mapreduce-shared-"
-        with tempfile.TemporaryDirectory(prefix=prefix) as tmpdir:
-            # everything in here so this `with` block doesn't end 
-            # until the Manager shuts down.
+        with tempfile.TemporaryDirectory(prefix="mapreduce-shared-") as tmpdir:
             self.tmpdir = tmpdir
-            LOGGER.info("Created tmpdir %s", self.tmpdir)
+            # LOGGER.info("Created tmpdir %s", self.tmpdir)
 
-            # threading
-            heartbeat_server_thread = threading.Thread(target=manager_udp_server, args=(self, host, port), daemon=True)
-            manager_tcp_server_thread = threading.Thread(target=manager_tcp_server, args=(self, host, port), daemon=True)
-            dead_worker_checking_thread = threading.Thread(target=check_heartbeats, args=(self,), daemon=True)
-            job_queue_thread = threading.Thread(target=job_queue, args=(self,), daemon=True)
+            heartbeat_server_thread = threading.Thread(
+                target=manager_udp_server,
+                args=(self, host, port),
+                daemon=True,
+            )
 
-            self.threads.extend([heartbeat_server_thread, 
-                                 manager_tcp_server_thread, 
-                                 dead_worker_checking_thread, 
-                                 job_queue_thread])
+            manager_tcp_server_thread = threading.Thread(
+                target=manager_tcp_server,
+                args=(self, host, port),
+                daemon=True,
+            )
+
+            dead_worker_checking_thread = threading.Thread(
+                target=check_heartbeats,
+                args=(self,),
+                daemon=True,
+            )
+
+            job_queue_thread = threading.Thread(
+                target=job_queue,
+                args=(self,),
+                daemon=True,
+            )
+
+            self.threads.extend([
+                heartbeat_server_thread,
+                manager_tcp_server_thread,
+                dead_worker_checking_thread,
+                job_queue_thread
+            ])
 
             for thread in self.threads:
                 thread.start()
 
             try:
-                self.shutdown_event.wait()  # Wait until shutdown signal
+                self.shutdown_event.wait()
             except KeyboardInterrupt:
                 LOGGER.info("Shutdown signal received.")
             finally:
                 LOGGER.info("Manager shutting down")
                 self.cleanup()
 
-            LOGGER.info("Cleaned up tmpdir %s", self.tmpdir)
+            # LOGGER.info("Cleaned up tmpdir %s", self.tmpdir)
 
-    # ------------------------------- Job Creation and Initialization -------------------------------
     def get_available_worker(self):
-        """ Get the next available worker. """
-        LOGGER.info("Checking for available worker...")
+        """Get the next available worker."""
+        # LOGGER.info("Checking for available worker...")
         for worker in self.workers.keys():
-            if worker not in self.busy_workers.keys() and worker not in self.dead_workers:
-                LOGGER.info(f"Worker {worker} is available.")
+            if (
+                worker not in self.busy_workers
+                and worker not in self.dead_workers
+            ):
+                # LOGGER.info(f"Worker {worker} is available.")
                 return worker
         LOGGER.info("No available workers.")
-        return None  # All workers are dead    
-    # ------------------------------- Worker Register and Shutdown and Listening Commands -------------------------------
+        return None
+
     def forward_shutdown_to_workers(self):
         """Send shutdown message to all registered workers."""
-        LOGGER.debug(f"Forwarding shutdown to workers.")
+        LOGGER.debug("Forwarding shutdown to workers.")
         message = {"message_type": "shutdown"}
         for worker_host, worker_port in self.workers.keys():
             worker = (worker_host, worker_port)
@@ -99,15 +124,20 @@ class Manager:
                 continue
             try:
                 if tcp_client(worker_host, worker_port, message):
-                    LOGGER.info(f"Sent shutdown message to worker {worker_host}:{worker_port}")
+                    LOGGER.info(
+                        f"Sent shutdown to worker {worker_host}:{worker_port}"
+                    )
                 else:
-                    LOGGER.info(f"tcp client failed to sent shutdown message to worker {worker_host}:{worker_port}")
+                    LOGGER.info(
+                        f"tcp fail sd msg to wrkr {worker_host}:{worker_port}"
+                    )
             except Exception as e:
-                LOGGER.warning(f"Failed to send shutdown to worker {worker_host}:{worker_port}: {e}")
-    # ------------------------------- Cleanup Function -------------------------------
+                LOGGER.warning(
+                    f"failed send sd to wrkr {worker_host}:{worker_port}: {e}"
+                )
+
     def cleanup(self):
-        """ Cleanup resources on shutdown. """
-        # self.shutdown_event.set() - already calls on msg receive
+        """Cleanup resources on shutdown."""
         for thread in self.threads:
             thread.join(timeout=2)
             if thread.is_alive():
@@ -115,6 +145,7 @@ class Manager:
 
         LOGGER.info("All threads have completed.")
         sys.exit(0)
+
 
 @click.command()
 @click.option("--host", "host", default="localhost")
--- mapreduce/manager/fuckingmaininputmapreduce.py
+++ mapreduce/manager/fuckingmaininputmapreduce.py
@@ -1,32 +1,38 @@
-from pathlib import Path
-from mapreduce.manager.job import *
-from mapreduce.utils.servers import *
-from mapreduce.manager.myheart import *
-import time
-import json
+"""MapReduce program."""
 import os
+import time
+# import json
 import shutil
+import logging
+from pathlib import Path
+
+# from mapreduce.manager.job import Job
+from mapreduce.utils.servers import tcp_client
+from mapreduce.manager.myheart import handle_dead_worker
 
 LOGGER = logging.getLogger(__name__)
+
+
 def input_partitioning(input_dir, num_mappers):
-    """
-    return a list of partitions
-    """
+    """Return a list of partitions."""
     LOGGER.info("Partitioning input files.")
     input_files = sorted(Path(input_dir).glob("*"))
-    input_files.sort()  # sort by name
-    partitions = [[] for _ in range(num_mappers)]  # Create empty lists for each mapper
+    input_files.sort()  # Sort by name
+    partitions = [[] for _ in range(num_mappers)]
 
     for idx, file in enumerate(input_files):
-        partitions[idx % num_mappers].append(file)  # round robin assignment
-    # PARTITIONS ARE LIST OF LIST OF PATHS
-    string_partitions = [[str(path) for path in partition] for partition in partitions]
-    LOGGER.info(f"Partitioned input files: {string_partitions}")
+        partitions[idx % num_mappers].append(file)  # Round robin assignment
+
+    string_partitions = [
+        [str(path) for path in partition] for partition in partitions
+    ]
+    # LOGGER.info(f"Partitioned input files: {string_partitions}")
     return string_partitions
 
+
 def start_mapping(self, string_partitions):
+    """Map."""
     for task_id, input_paths in enumerate(string_partitions):
-        # add all tasks to job queue
         task_message = {
             "message_type": "new_map_task",
             "task_id": task_id,
@@ -36,51 +42,53 @@ def start_mapping(self, string_partitions):
             "num_partitions": self.job.num_reducers,
         }
         self.job.add_task(task_id, task_message)
-    LOGGER.info("Added all partitions to job tasks list!!")
+    # LOGGER.info("Added all partitions to job tasks list!")
 
-    # tcp listener listens for completion msg, job notifies all
-    LOGGER.info(f"NOTIFS: {self.shutdown_event.is_set()}, {self.job.mapping_finished_event.is_set()}")
-    while not self.shutdown_event.is_set() and not self.job.mapping_finished_event.is_set():
+    # LOGGER.info(f"NOTIFS: {self.shutdown_event.is_set()}, "
+    #             f"{self.job.mapping_finished_event.is_set()}")
+    while (
+        not self.shutdown_event.is_set() and
+        not self.job.mapping_finished_event.is_set()
+    ):
         worker = self.get_available_worker()
-        if worker: # worker available
+        if worker:
             LOGGER.info("Worker available for task.")
-            full = self.job.get_task() # try getting task
-
-            if full: # task and worker available. assigning
+            full = self.job.get_task()
+            if full:
                 task_id, task = full
-                LOGGER.info(f"MANAGER MAPPING: SENDING TASK {task} of TYPE {type(task)} TO WORKER {worker}")
+                # LOGGER.info(f"MANAGER MAPPING: SENDING TASK {task} "
+                #             f"of TYPE {type(task)} TO WORKER {worker}")
                 whost, wport = worker
 
-                if tcp_client(whost, wport, task): # turns task dict into json and sends
-                    LOGGER.info(f"TASK {task} SENT TO WORKER {worker}")
-                    # just keeping  track below
+                if tcp_client(whost, wport, task):
+                    # LOGGER.info(f"TASK {task} SENT TO WORKER {worker}")
                     self.job.assign_task(task_id, worker)
                     self.busy_workers[worker] = None
                 else:
                     handle_dead_worker(self, worker)
-                    # worker dead
 
-        LOGGER.info("waiting for worker or task in manager mapping")
+        # LOGGER.info("Waiting for worker or task in manager mapping")
         time.sleep(1)
-    LOGGER.info("EXITED START_MAPPING WHILE LOOP")
+    # LOGGER.info("Exited start_mapping while loop")
+
 
 def start_reducing(self):
-    LOGGER.info("MANAGER: REDUCER RUNNING")
-    # eg in /tmp/mapreduce-shared-XYZ123/job-00000
-    # the maptask00000-part00000 stuff
+    """Reduce."""
+    # LOGGER.info("MANAGER: REDUCER RUNNING")
     tmpdir = self.job.jobspecifictmpdir
     map_outputs = sorted(os.listdir(tmpdir))
-    LOGGER.info(f"MAP OUTPUTS: {map_outputs}")
+    # LOGGER.info(f"MAP OUTPUTS: {map_outputs}")
 
     partitions = {}
     for file_name in map_outputs:
         if "part" in file_name:
-            partition_id = file_name.split("-part")[-1]  # partition number
-            partitions.setdefault(partition_id, []).append(os.path.join(tmpdir, file_name)) # create partitions
-    LOGGER.info(f"REDUCING PARTITIONS MADE: {partitions}")
+            partition_id = file_name.split("-part")[-1]
+            partitions.setdefault(partition_id, []).append(
+                os.path.join(tmpdir, file_name)
+            )
+    # LOGGER.info(f"REDUCING PARTITIONS MADE: {partitions}")
 
-    # update job
-    self.job.post_map_reset() # need to clear it out
+    self.job.post_map_reset()
 
     for task_id, partition in partitions.items():
         task_id = int(task_id)
@@ -92,73 +100,71 @@ def start_reducing(self):
             "output_directory": self.job.output_dir,
         }
         self.job.add_task(task_id, task_message)
-    LOGGER.info("REDUCING tasks added to job class")
+    # LOGGER.info("Reducing tasks added to job class")
 
-    # assigning partitions
-    while not self.shutdown_event.is_set() and not self.job.reducing_finished_event.is_set():
+    while (
+        not self.shutdown_event.is_set() and
+        not self.job.reducing_finished_event.is_set()
+    ):
         worker = self.get_available_worker()
         if worker:
-            LOGGER.info("Worker available for reducing task.")
-            full = self.job.get_task() # try getting task
+            # LOGGER.info("Worker available for reducing task.")
+            full = self.job.get_task()
             if full:
                 task_id, task = full
-                LOGGER.info(f"MANAGER REDUCING: TASK AND WORKER AVAILABLE. SENDING TASK {task} TO WORKER {worker}")
+                # LOGGER.info(f"MANAGER REDUCING: TASK AND WORKER AVAILABLE. "
+                #             f"SENDING TASK {task} TO WORKER {worker}")
                 whost, wport = worker
 
                 if tcp_client(whost, wport, task):
-                    LOGGER.info(f"TASK {task} SENT TO WORKER {worker}")
+                    # LOGGER.info(f"TASK {task} SENT TO WORKER {worker}")
                     self.job.assign_task(task_id, worker)
-                    # MARK AS BUSY IN SELF
-                    LOGGER.info("THROUGH")
+                    # LOGGER.info("Through")
                     self.busy_workers[worker] = None
                 else:
                     handle_dead_worker(self, worker)
-                    
 
-        LOGGER.info("waiting for worker or task in manager reducing")
+        # LOGGER.info("Waiting for worker or task in manager reducing")
         time.sleep(1)
-    LOGGER.info("EXITED START_REDUCING WHILE LOOP")
+    # LOGGER.info("Exited start_reducing while loop")
+
 
 def job_queue(self):
+    """Jq."""
     while not self.shutdown_event.is_set():
         with self.new_job_alert_condition:
-            while not self.job_queue:  # If the job queue is empty, wait
+            while not self.job_queue:
                 self.new_job_alert_condition.wait()
-                LOGGER.info("New job added, new_job_alert_condition alerted")
+                # LOGGER.info("New job added, new_job_alert_condition alerted")
 
-            # job available
             self.job = self.job_queue.popleft()
 
-            # FINAL OUTPUT DIRECTORY
             output_dir = self.job.output_dir
             if os.path.exists(output_dir):
                 shutil.rmtree(output_dir)
             os.makedirs(output_dir)
-            LOGGER.info(f"Created output directory: {output_dir}")
+            # LOGGER.info(f"Created output directory: {output_dir}")
 
-            # JOB RELATED TEMP OUTPUTS
             job_tmpdir = f"job-{self.job.job_id:05d}"
             job_tmpdir_path = Path(self.tmpdir) / job_tmpdir
             self.job.jobspecifictmpdir = str(job_tmpdir_path)
             os.makedirs(job_tmpdir_path, exist_ok=True)
-            LOGGER.info(f"Created temporary directory for job \
-                        {self.job.job_id}: {job_tmpdir_path}")
+            # LOGGER.info(f"Created temporary directory for job "
+            #             f"{self.job.job_id}: {job_tmpdir_path}")
 
-            # INPUT PARTITION
-            string_partitions = input_partitioning(self.job.input_dir,
-                                                   self.job.num_mappers)
-            # SEND PARTITION RESULT TO MAPPER
+            string_partitions = input_partitioning(
+                self.job.input_dir, self.job.num_mappers
+            )
             start_mapping(self, string_partitions)
             self.job.post_map_reset()
-            LOGGER.info(f"BUSY WORKERS: {self.busy_workers.keys()} \n \n \n")
-            LOGGER.info("Busy workers should be removed after map reset!")
+            # LOGGER.info(f"BUSY WORKERS: {self.busy_workers.keys()}")
+            # LOGGER.info("Busy workers should be removed after map reset!")
             self.busy_workers.clear()
             start_reducing(self)
 
-            LOGGER.info("DONE REDUCING! \n \n \n \n \n")
-            # REDUCING DONE - CLEANUP
+            # LOGGER.info("DONE REDUCING!")
             if os.path.exists(job_tmpdir_path):
                 shutil.rmtree(job_tmpdir_path)
-                LOGGER.info(f"Deleted temporary directory for job \
-                            {self.job.job_id}: {job_tmpdir_path}")
+                # LOGGER.info(f"Deleted temporary directory for job "
+                        # f"{self.job.job_id}: {job_tmpdir_path}")
         time.sleep(1)
