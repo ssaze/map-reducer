0a3320833136cc041fb347002a1052ba5cb7a3e4
ssaze <saze@umich.edu>
2025-03-29 23:25:39 -0400

    hello

--- mapreduce/worker/__main__.py
+++ mapreduce/worker/__main__.py
@@ -262,27 +262,27 @@ class Worker:
         LOGGER.info(f"Finished {msg['message_type']}")
 
 
+
+
 @click.command()
 @click.option("--host", "host", default="localhost")
-@click.option("--port", "port", default=6000)
+@click.option("--port", "port", default=6001)
+@click.option("--manager-host", "manager_host", default="localhost")
+@click.option("--manager-port", "manager_port", default=6000)
 @click.option("--logfile", "logfile", default=None)
 @click.option("--loglevel", "loglevel", default="info")
-@click.option("--shared_dir", "shared_dir", default=None)
-def main(host, port, logfile, loglevel, shared_dir):
-    """Run Manager."""
-    tempfile.tempdir = shared_dir
+def main(host, port, manager_host, manager_port, logfile, loglevel):
+    """Run Worker."""
     if logfile:
         handler = logging.FileHandler(logfile)
     else:
         handler = logging.StreamHandler()
-    formatter = logging.Formatter(
-        f"Manager:{port} [%(levelname)s] %(message)s"
-    )
+    formatter = logging.Formatter(f"Worker:{port} [%(levelname)s] %(message)s")
     handler.setFormatter(formatter)
     root_logger = logging.getLogger()
     root_logger.addHandler(handler)
     root_logger.setLevel(loglevel.upper())
-    Manager(host, port)
+    Worker(host, port, manager_host, manager_port)
 
 
 if __name__ == "__main__":
